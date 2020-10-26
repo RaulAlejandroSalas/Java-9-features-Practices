b2870c592cfb50ea39f5518f0112ebdd04d41976
unknown <RSAL182@cc-college.gecs.loc>
2020-10-26 11:43:13 +0100

    [ADD] First Commit Spring Boot Reactor Demo

---
+++ spring-boot-reactor-demo/src/main/java/de/rauldev/springbootreactordemo/SpringBootReactorDemoApplication.java
@@ -0,0 +1,13 @@
+package de.rauldev.springbootreactordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorDemoApplication.class, args);
+	}
+
+}
---
+++ spring-boot-reactor-demo/src/test/java/de/rauldev/springbootreactordemo/SpringBootReactorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rauldev.springbootreactordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
