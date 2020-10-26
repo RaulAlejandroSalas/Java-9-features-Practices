b26c468a05ed9f4c90f72762a95e7edb6e80122a
unknown <RSAL182@cc-college.gecs.loc>
2020-10-26 12:41:41 +0100

    [ADD] Adding example reactor dependence

--- spring-boot-reactor-demo/src/main/java/de/rauldev/springbootreactordemo/SpringBootReactorDemoApplication.java
+++ spring-boot-reactor-demo/src/main/java/de/rauldev/springbootreactordemo/SpringBootReactorDemoApplication.java
@@ -1,13 +1,26 @@
 package de.rauldev.springbootreactordemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
+
+import reactor.core.publisher.Flux;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootReactorDemoApplication {
+public class SpringBootReactorDemoApplication  implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorDemoApplication.class, args);
 	}
+
+	@Override
+	public void run(String ... args){
+		Flux<String> flowString = Flux.just("Raul","Victor","Verenice","Diana")
+									  .doOnNext(System.out::println);
+		flowString.subscribe();
+	}
+
+
 
 }
